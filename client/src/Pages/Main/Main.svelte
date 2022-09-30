aede9af66fa30571a344024afdf117d007c089f0
Valentin <93974733+valentindush@users.noreply.github.com>
2022-09-30 09:15:13 +0200

    finished home page

--- client/src/Pages/Main/Main.svelte
+++ client/src/Pages/Main/Main.svelte
@@ -40,14 +40,26 @@
 
 
     ]
+    const categories:string[] = [
+        "Painting",
+        "3D arts",
+        "Music",
+        "Photography",
+        "sculpture",
+        "Drawing",
+    ]
+    let activeCat:string = categories[0]
+    const chooseCategory = (value:string)=>{
+        activeCat = value;
+    }
 </script>
 
-<main class="h-screen px-12 overflow-auto">
-    <header class="sticky top-0">
+<main class="h-screen px-12 overflow-auto relative">
+    <header class="sticky top-0 backdrop-blur-sm">
         <div class="logo pt-4">
             <img src="./images/logo.svg" class="scale-75" alt="">
         </div>
-        <div class="mt-4 absolute right-4 flex items-center gap-12">
+        <div class="mt-4 absolute sm:static right-4 flex items-center gap-12 z-10">
             <div class="search w-[35rem] relative">
                 <input type="text" placeholder="Search arts" class="w-full bg-white rounded-full py-2 px-3 pl-14 backdrop-blur-md bg-opacity-20 text-white placeholder:text-white mt-2 outline-none " />
                 <Icon src={FiSearch} size="24" color="white" className="absolute left-5 top-4" />
@@ -72,7 +84,7 @@
             <div class="title">
                 <h2 class="text-gray-200 text-2xl">Now trending</h2>
             </div>
-            <div class=" sm:block flex items-center justify-between gap-1 flex-wrap py-3">
+            <div class=" sm:block flex items-center justify-start gap-1 flex-wrap py-3">
                 {#each arts as art}
                     <Art {art} />
                     {:else}
@@ -80,9 +92,49 @@
                         
                 {/each}
             </div>
+            
+        </div>
+        <div class="mt-4">
+            <div class="title">
+                <h2 class="text-gray-200 text-2xl">Most popular</h2>
+            </div>
+            <div class=" sm:block flex items-center justify-start gap-1 flex-wrap py-3">
+                {#each arts as art}
+                    <Art {art} />
+                    {:else}
+                        <p class="text-red-500 p-2">Something went wrong</p>
+                        
+                {/each}
+            </div>
+            
+        </div>
+        <div class="mt-4">
+            <div class="title">
+                <h2 class="text-gray-200 text-2xl">Choose by categories</h2>
+                <div class="catgrs py-4">
+                    {#each categories as category}
+                        <button on:click={()=>chooseCategory(category)} class={`py-2 px-5 rounded-full ${activeCat === category?"bg-pink-500 bg-opacity-60":"bg-white bg-opacity-20"} inline-block mr-2 cursor-pointer active:scale-[.97] border border-pink-500 border-opacity-50 mt-3`}>
+                            <p class="text-gray-200 text-center">{category}</p>
+                        </button>
+                    {/each}
+                </div>
+            </div>
+            <div class=" sm:block flex items-center justify-start gap-1 flex-wrap py-3">
+                {#each arts as art}
+                    <Art {art} />
+                    {:else}
+                        <p class="text-red-500 p-2">Something went wrong</p>
+                        
+                {/each}
+            </div>
+            
         </div>
 
     </div>
+
+    <button class="fixed z-50 w-[50px] h-[50px] rounded-full bg-pink-500">
+
+    </button>
 
 </main>
 <style>
--- client/src/lib/Art.svelte
+++ client/src/lib/Art.svelte
@@ -6,16 +6,17 @@ export let art : {
     image: string,
     };
 </script>
-<div class="max-h-[250px] h-[250px] w-[280px] sm:w-full">
+<div class="max-h-[250px] h-[250px] w-[280px] sm:w-full sm:mt-2">
     <a href={`#/art/${art.id}`} class="">
         <div class="h-[83%]">
             <img src={art.image} class="w-full h-full object-cover rounded-lg" alt="">
         </div>
-        <div class="h-[15%] mt-2 bg-black bg-opacity-30 rounded-full">
+        <div class="h-[15%] mt-2 bg-black bg-opacity-30 rounded-full backdrop-blur-sm z-0">
             <div class="flex justify-around items-center h-full text-gray-300">
                 <p>{art.title}</p>
                 <p>{art.owner}</p>
             </div>
         </div>
+        
     </a>
 </div>
