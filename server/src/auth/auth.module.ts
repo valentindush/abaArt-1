8acd4f8e04a4593e314a977c27c8a8332af71765
Valentin <93974733+valentindush@users.noreply.github.com>
2022-12-17 11:56:22 +0200

    UPDT: sign in functions

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule],
   controllers: [AuthController],
   providers: [AuthService, PrismaService]
 })
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -3,13 +3,19 @@ import { PrismaService } from 'prisma/prisma.service';
 import { SigninDto } from './dto/signin.dto';
 import { SignUPdto } from './dto/signup.dto';
 import * as bcrypt from 'bcrypt'
+import { JwtService } from '@nestjs/jwt';
+import { jwtSecret } from 'src/utils/constants';
+import { sign } from 'crypto';
 
 @Injectable()
 export class AuthService {
-   constructor(private prisma: PrismaService){}
+   constructor(private prisma: PrismaService, private jwt: JwtService){}
    async signup(body: SignUPdto) {
+
       const {email, password, fullName} = body
+
       const foundUser = await this.prisma.user.findUnique({where: {email: email}})
+
       if(foundUser){
          throw new BadRequestException('Email is already taken')
       }
@@ -33,8 +39,29 @@ export class AuthService {
       }
    }
    async signin(body: SigninDto) {
+      const {email, password} = body
+
+      const foundUser = await this.prisma.user.findUnique({where: {email: email}})
+
+      if(!foundUser){
+         throw new BadRequestException('Incorrenct email or password')
+      }
+      const isMatch = await this.comparePasswords({password, hash:foundUser.hashedPassword})
+
+      if(!isMatch){
+         throw new BadRequestException("Incorrect email or password")
+      }
+
+      const signed = await this.signToken({id: foundUser.id, email:foundUser.email})
+
+      return {
+         status: true,
+         message: "Login Successfull",
+         token: signed
+      }
 
    }
+
    async signout(){
 
    }
@@ -42,5 +69,16 @@ export class AuthService {
   async hashPassword (password:string){
       const saltRounds = 10
       return await bcrypt.hash(password, saltRounds)
+  }
+
+  async comparePasswords(args: {password:string, hash:string}){
+      return await bcrypt.compare(args.password,args.hash)
+  }
+
+  async signToken(args: {id: string, email: string}){
+   const payload = args
+   return this.jwt.signAsync(payload, {
+      secret: jwtSecret
+   })
   }
 }
