767c85a25b63aca07fe073f4d2aa9ba0e8696822
Valentin <93974733+valentindush@users.noreply.github.com>
2022-12-19 21:38:02 +0200

    JWT functions

--- server/src/post/post.module.ts
+++ server/src/post/post.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule],
   controllers: [PostController],
   providers: [PostService]
 })
--- server/src/post/post.service.ts
+++ server/src/post/post.service.ts
@@ -1,17 +1,25 @@
 import { Injectable , HttpException, HttpStatus} from '@nestjs/common';
 import { Request } from 'express';
 import { PostDto } from './dto/postDTO';
+import { JwtService } from '@nestjs/jwt';
+import { jwtSecret } from 'src/utils/constants';
 
 
 @Injectable()
 export class PostService {
+
+    constructor(private jwt: JwtService){}
+
     createPost(file: Express.Multer.File,req:Request,body:PostDto){
         if(!file || !this.validateFile(file)){
             throw new HttpException('File must be an image not more than 10mbs ', HttpStatus.BAD_REQUEST)
         }
 
-        //Save into the DB
+        //GET token
+
         
+        //Save into the DB
+
 
     }
 
@@ -27,5 +35,13 @@ export class PostService {
     }
     generateRandomFileName(){
         return `abaart-${Date.now()}-${Math.random() * 1000000}`
+    }
+
+    async validateToken(token: string){
+        return await this.jwt.verify(token)
+    }
+
+    decodeJwt(token: string){
+        return this.jwt.decode(token)
     }
 }
